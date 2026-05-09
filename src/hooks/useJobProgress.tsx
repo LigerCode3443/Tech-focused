1151685d90ecf142a4e51fd702eaef960303ffb3
LigerCode3443 <markevch.sit@gmail.com>
2026-05-09 23:12:24 +0300

    fix socket

--- src/hooks/useJobProgress.tsx
+++ src/hooks/useJobProgress.tsx
@@ -10,7 +10,7 @@ export const useJobProgress = () => {
 
   useEffect(() => {
     const newSocket = io(SOCKET_URL, {
-      transports: ["polling", "websocket"],
+      transports: ["websocket"],
       withCredentials: true,
       reconnectionAttempts: 5,
       timeout: 10000,
